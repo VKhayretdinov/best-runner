d9cedacee0ccf447ed55605c43069e4fa836caa5
Vladislav Khayretdinov <vladislav.khayretdinov@gmail.com>
2020-02-09 22:01:25 +0700

    add alert on sing in error

--- client/src/containers/SignInContainer/index.jsx
+++ client/src/containers/SignInContainer/index.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Alert } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import { fetchSignIn } from './redux/actions';
@@ -12,6 +12,7 @@ class SignInContainer extends Component {
     history: PropTypes.shape({
       push: PropTypes.func.isRequired,
     }).isRequired,
+    isError: PropTypes.bool.isRequired,
   };
 
   handleSignInFormSubmit = (formValues) => {
@@ -20,19 +21,28 @@ class SignInContainer extends Component {
 
   render() {
     return (
-      <Row className="justify-content-center">
-        <Col xs={4}>
-          <SignIn
-            onSubmit={this.handleSignInFormSubmit}
-          />
-        </Col>
-      </Row>
+      <Fragment>
+        <Row className="justify-content-center">
+          <Col xs={4}>
+            <SignIn
+              onSubmit={this.handleSignInFormSubmit}
+              isError={this.props.isError}
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-3">
+          <Col xs={4}>
+            {this.props.isError && (<Alert>User not found.</Alert>)}
+          </Col>
+        </Row>
+      </Fragment>
     );
   }
 }
 
 const mapStateToProps = state => ({
   isLogged: !!state.currentUser.data,
+  isError: !!state.signIn.error,
 });
 
 export default connect(mapStateToProps, { fetchSignIn })(withRouter(SignInContainer));

42e18f7dc0f62fb116a9c9cf2b6446399946b310
Vladislav Khayretdinov <vladislav.khayretdinov@gmail.com>
2020-02-09 22:04:42 +0700

    add alert on sing up error

--- client/src/containers/SignUpContainer/index.jsx
+++ client/src/containers/SignUpContainer/index.jsx
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Alert } from 'reactstrap';
 import SignUp from './components/SignUp';
 import { fetchSignUp } from './redux/actions';
 
 class SignUpContainer extends Component {
   static propTypes = {
     fetchSignUp: PropTypes.func.isRequired,
+    isError: PropTypes.bool.isRequired,
   };
 
   handleSignUpFormSubmit = formValues => this.props.fetchSignUp(formValues);
 
   render() {
     return (
-      <Row className="justify-content-center">
-        <Col xs={4}>
-          <SignUp
-            onSubmit={this.handleSignUpFormSubmit}
-          />
-        </Col>
-      </Row>
+      <Fragment>
+        <Row className="justify-content-center">
+          <Col xs={4}>
+            <SignUp
+              onSubmit={this.handleSignUpFormSubmit}
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-3">
+          <Col xs={4}>
+            {this.props.isError && (<Alert>User not found.</Alert>)}
+          </Col>
+        </Row>
+      </Fragment>
     );
   }
 }
 
-export default connect(null, { fetchSignUp })(SignUpContainer);
+const mapStateToProps = state => ({
+  isError: !!state.signUp.error,
+});
+
+export default connect(mapStateToProps, { fetchSignUp })(SignUpContainer);
